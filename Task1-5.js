051ce963581b6ac81b2fd57578b152a999733d6a
Anita Daniel <anitadaniel1977@gmail.com>
2026-04-15 15:38:51 +0200

    Add for and while loop practice tasks 1-5

---
+++ Task1-5.js
@@ -0,0 +1,29 @@
+for(let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+console.log('Counting completed!');
+
+for(let i = 5; i >= 1; i--) {
+    console.log(i);
+}
+console.log('Countdown finished!');
+
+let i = 1;
+while(i <= 5) {
+    console.log(i);
+    i++;
+}
+console.log('Counting completed!');
+
+let i = 5;
+while(i >= 1) {
+    console.log(i);
+    i--;
+}
+console.log('Countdown finished!');
+
+let i = 2018;
+while(i <= 2022) {
+    console.log(i);
+    i++;
+}
