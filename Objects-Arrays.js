8c1eb93e0f5c83c6bce6acd22c05858c01cefd25
Anita Daniel <anitadaniel1977@gmail.com>
2026-04-28 10:26:21 +0200

    Add arrays and objects exercise with comments

---
+++ Objects-Arrays.js
@@ -0,0 +1,31 @@
+// Task 1: Create a new empty array literal
+var clothes = [];
+
+// Task 2: Add 5 clothing items using push()
+clothes.push("Coat");
+clothes.push("Thermal Leggings");
+clothes.push("Thick Socks");
+clothes.push("Turtleneck");
+clothes.push("Cotton Underwear");
+
+// Task 3: Remove the fifth item using pop()
+clothes.pop(); // removes "Cotton Underwear"
+console.log(clothes); // ['Coat', 'Thermal Leggings', 'Thick Socks', 'Turtleneck']
+
+// Task 4: Add a new piece of clothing using push()
+clothes.push("Bra");
+
+// Task 5: Log the third item from the array (index 2)
+console.log(clothes[2]); // Thick Socks
+
+// Task 6: Create a new empty object literal
+var favCar = {};
+
+// Task 7: Add a color property using dot notation
+favCar.color = "Black";
+
+// Task 8: Add a convertible property with a boolean value
+favCar.convertible = true;
+
+// Task 9: Log the entire favCar object
+console.log(favCar); // {color: 'Black', convertible: true}
